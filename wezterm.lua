cd8a7d54b09e8a8c8186d6436229c9ce6ce7da37
nmreadelf <frank.q.kong@hotmail.com>
2022-03-24 11:31:01 +0800

    add about tab short cuts set terminal color set font and font size

--- wezterm.lua
+++ wezterm.lua
@@ -3,6 +3,9 @@ local mykeys = {
     {key="C", mods="ALT", action="Copy"},
     {key="V", mods="ALT", action="Paste"},
     {key="t", mods="ALT", action=wezterm.action{SpawnCommandInNewTab={ args={"bash"} }}},
+    {key="]", mods="ALT", action=wezterm.action{ActivateTabRelative=1}},
+    {key="[", mods="ALT", action=wezterm.action{ActivateTabRelative=-1}},
+    {key="w", mods="ALT", action=wezterm.action{CloseCurrentTab={confirm=true}}},
 }
 
 for i = 1, 8 do
@@ -15,5 +18,8 @@ end
 
 return {
   keys = mykeys,
+  -- color_scheme = "Monokai Remastered",
+  color_scheme = "Solarized Dark Higher Contrast",
+  font = wezterm.font("Consolas"),
+  font_size = 20.0,
 }
-
