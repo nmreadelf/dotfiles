51d2478c2bc44c3e7ccd2bbb259eb47629ab6169
nmreadelf <frank.q.kong@hotmail.com>
2022-03-25 18:47:43 +0800

    add os system judge for mainKey

--- wezterm.lua
+++ wezterm.lua
@@ -1,17 +1,29 @@
 local wezterm = require 'wezterm';
+local mainKey = "ALT"
+
+local BinaryFormat = package.cpath:match("%p[\\|/]?%p(%a+)")
+if BinaryFormat == "dll" then
+    mainKey = "META"
+elseif BinaryFormat == "dylib" then
+    mainKey = "META"
+else
+    mainKey = "ALT"
+end
+
+local SHELL = "bash"
 local mykeys = {
-    {key="C", mods="ALT", action="Copy"},
-    {key="V", mods="ALT", action="Paste"},
-    {key="t", mods="ALT", action=wezterm.action{SpawnCommandInNewTab={ args={"bash"} }}},
-    {key="]", mods="ALT", action=wezterm.action{ActivateTabRelative=1}},
-    {key="[", mods="ALT", action=wezterm.action{ActivateTabRelative=-1}},
-    {key="w", mods="ALT", action=wezterm.action{CloseCurrentTab={confirm=true}}},
+    {key="C", mods=mainKey, action="Copy"},
+    {key="V", mods=mainKey, action="Paste"},
+    {key="t", mods=mainKey, action=wezterm.action{SpawnCommandInNewTab={ args={SHELL} }}},
+    {key="]", mods=mainKey, action=wezterm.action{ActivateTabRelative=1}},
+    {key="[", mods=mainKey, action=wezterm.action{ActivateTabRelative=-1}},
+    {key="w", mods=mainKey, action=wezterm.action{CloseCurrentTab={confirm=true}}},
 }
 
 for i = 1, 8 do
   table.insert(mykeys, {
     key=tostring(i),
-    mods="ALT",
+    mods=mainKey,
     action=wezterm.action{ActivateTab=i-1},
   })
 end
